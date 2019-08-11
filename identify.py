c8046475c35c6b81faa74bfbd0fdd572e515c392
Wistral <whistral@gmail.com>
2019-08-11 20:34:44 +0800

    password no feedback

--- identify.py
+++ identify.py
@@ -1,43 +1,75 @@
 import requests
-from info_parse import *
 from bs4 import BeautifulSoup
+from os import path
+import getpass
 
 
-ac, psw = login()
-raw_headers = """POST /0.htm HTTP/1.1
-Host: 172.18.2.2
-Connection: kep-alive
-Content-Length: 67
-Pragma: no-cache
-Cache-Control: no-cache
-Origin: http://172.18.3.3
-Upgrade-Insecure-Requests: 1
-Content-Type: application/x-www-form-urlencoded
-User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
-Referer: http://172.18.3.3/0.htm
-Accept-Encoding: gzip, deflate
-Accept-Language: zh-TW,zh-CN;q=0.9,zh;q=0.8,en-GB;q=0.7,en;q=0.6,ja;q=0.5
-"""
-
-raw_data = """
-DDDDD: {}
-upass: {}
-0MKKey: %B5%C7+%C2%BC%A1%A1Login
-v6ip: 
-""".format(ac, psw)
-
-headers = parse_headers(raw_headers)
-data = parse_headers(raw_data)
-html = requests.post('http://172.18.3.3/0.htm', headers=headers, data=data)
-res = BeautifulSoup(html.text, 'lxml')
-msg = res.title.string
-if msg == '登录成功':
-    print(msg)
-else:
-    print('登录失败')
-    with open('info.txt', 'w') as f:
-        f.close()
-# print(res.title.string)
+# user-info file name
+USER_INFO_CONF = 'info.dat'
 
 
+def login(store=True):  # -> str, str
+    """
+    get account and password
+    It will ask for account and password and stored on first time.
+    
+    \param store whether to store user information
+    \return account[str], password[str]
+    """
+    # user info file exists and not empty
+    if path.exists(USER_INFO_CONF) and path.getsize(USER_INFO_CONF):
+        with open(USER_INFO_CONF, 'r') as f:
+            ans = [_ for _ in f]
+            return ans[:2]
+    else:
+        with open(USER_INFO_CONF, 'wb') as f:
+            account = input('请输入学号/帐号:\n')
+            password = getpass.getpass('请输入密碼(无回显):\n')
+            if store:
+                f.write('\n'.join([account, password]).encode())
+            return account, password
+
+
+def connect_internet():
+    """
+    connect to Internet from inner school net
+    """
+    # acqurie account and password
+    account, psw = login()
+    headers = {
+        'Host': '172.18.2.2',
+        'Connection': 'kep-alive',
+        'Content-Length': '67',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+        'Origin': 'http://172.18.3.3',
+        'Upgrade-Insecure-Requests': '1',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Referer': 'http://172.18.3.3/0.htm',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-TW,zh-CN;q=0.9,zh;q=0.8,en-GB;q=0.7,en;q=0.6,ja;q=0.5'
+    }
+
+    data = {
+        'DDDDD': account,
+        'upass': psw,
+        '0MKKey': '%B5%C7+%C2%BC%A1%A1Login',
+        'v6ip': ''
+    }
+    # make POST request
+    html = requests.post('http://172.18.3.3/0.htm', headers=headers, data=data)
+    res = BeautifulSoup(html.text, 'lxml')
+    msg = res.title.string
+    if msg == '登录成功':
+        print(msg)
+    else:
+        print('登录失败')
+        # clear incorrect info
+        with open(USER_INFO_CONF, 'w') as f:
+            pass
+
+
+if __name__ == "__main__":
+    connect_internet()
