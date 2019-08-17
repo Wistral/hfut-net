c3b6372d025b6431b02671ea2e5ca7d2cfda32c8
Wistral <whistral@gmail.com>
2019-08-18 01:34:43 +0800

    update script

--- hotspot-set-up.bash
+++ hotspot-set-up.bash
@@ -1,13 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 architecture=$(dpkg --print-architecture)
-# package hostapd from http://old-releases.ubuntu.com/ubuntu/pool/universe/w/wpa/
-which ap-hotspot && echo 'Dependency already satisfied!' \
-|| (echo 'Dependency not found!' && sudo apt-get install -y dnsmasq \
-&& sudo dpkg -i hostapd*${architecture}.deb \
-&& sudo apt-get install -y ap-hotspot \
-&& echo 'Install dependency successfully!')
-echo 'Start hotspot configuration'
-sudo ap-hotspot configure && echo 'Configuration finished, try to start...'
-sudo ap-hotspot start
+
+case "${architecture}" in
+    "amd64" | "i386" | "armhf" )
+        # package hostapd from http://old-releases.ubuntu.com/ubuntu/pool/universe/w/wpa/
+        which ap-hotspot && echo 'Dependency already satisfied!' \
+        || (echo 'Dependency not found!' && sudo apt-get install -y dnsmasq \
+        && sudo dpkg -i hostapd*${architecture}.deb \
+        && sudo apt-get install -fy ap-hotspot \
+        && echo 'Install dependency successfully!')
+        echo 'Start hotspot configuration...'
+        sudo ap-hotspot configure && echo 'Configuration finished, try to start...'
+        sudo ap-hotspot start
+        ;;
+    *)
+        echo "architecture ${architecture} is not supported yet! \nexit..."
+        ;;
+esac
 
